f695a59edb92b419dd4cb917a4c0eca92deb3b9e
Abhay-Lejith <abhaylejith@gmail.com>
2024-03-20 19:41:38 +0530

    comments cleaning up

--- log.c
+++ log.c
@@ -1,21 +1,3 @@
-/******************************************************************************
- *    Copyright 2012 André Gasser
- *
- *    This file is part of Dnsmap.
- *
- *    Dnsmap is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    Dnsmap is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with Dnsmap.  If not, see <http://www.gnu.org/licenses/>.
- *****************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,9 +5,6 @@
 #include <stdarg.h>
 #include "log.h"
 
-/* static reduces this variables scope to file scope.
- * This variable is global, but only throughout this file.
- */
 static int global_loglevel = LOG_INFO;
 
 void logline(int loglevel, const char* format, ...) 
@@ -60,9 +39,6 @@ void logline(int loglevel, const char* format, ...)
 	}
 }
 
-/*
- * This function sets the loglevel of the logger.
- */
 void set_loglevel(int loglevel)
 {
 	if ((loglevel == LOG_ERROR) || (loglevel == LOG_INFO) || (loglevel == LOG_DEBUG))
